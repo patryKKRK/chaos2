1ab0ba397575f1c6c5e4669f5bbfe2dc2791e116
patryKKRK <patryk.orlikowski@gmail.com>
2020-11-19 13:36:12 +0100

    public key variable added

--- key.tf
+++ key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj6ffQj/DzNKjf4yQ0jbHYns6/bk+DfmgkOLxeWo+neNZZdM6F8AaPO3iK+i1bnG1bryHcZiZ/hD9n+qKtcPVFiltC1xew5b1rWLuuMIutQqhVe99+RAUIy994YuTi7TUdzL4I0J1nqX/PYesVMoibTY0ozBX4MdENo+IXCUWF5accL+dlJTf9sIepXLTl8uKojNQ7nDAk5wZoyOzVJalybSnckyRipi4hkschRtf9B4TZbVDT4ZDhCSFEFWEzoVDXnwEn+TMWA3Y4jSbwsqt8TKA9DmMCKZav5L0aP32odseur16kKIaD5IYlxHurC9gmft4JlKpncCwFe0SCxcMdIH46GnhYhKomIO9gFZNjPmDQlv+LsLAZdAToS2Lt82C1b0ApSKvXeI5+LNjyYxhXeYwEJrLIFZPS3ui2r8L2qkGybTeUFlbRnWzDuzxMTO7aivgc1vRQwljA2KhKo+ITUJkrQnslVPjlKPZB3diaPfc5NuOVbWZocPQ6C3PL0Fs= adminp@Lin"
+  public_key = var.public_key
 }
+key_name   = "deployer-key"
